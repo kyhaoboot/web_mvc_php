c2b1ed4a6b53dbcdca3a137e29db7d318510298a
kyhaoboot <kyhaoboot@gmail.com>
2022-05-06 00:03:09 +0700

    first commit

---
+++ classes/adminlogin.php
@@ -0,0 +1,47 @@
+<?php
+    include '../lib/session.php';
+    Session::checkLogin();
+    include '../lib/database.php';
+    include '../helpers/format.php';
+
+?>
+
+<?php
+class adminlogin
+{
+    private $db;
+    private $fm;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+        $this->fm = new Format();    
+    }
+    public function login_admin($adminUser,$adminPass){
+        $adminUser = $this->fm->validation($adminUser);
+        $adminPass = $this->fm->validation($adminPass);
+
+        $adminUser = mysqli_real_escape_string($this->db->link,$adminUser);
+        $adminPass = mysqli_real_escape_string($this->db->link,$adminPass);
+    
+        if(empty($adminUser) || empty($adminPass)){
+            $alert = "User and Pass must be not emtpy";
+        }else{
+            $query = "select * from tbl_admin where adminUser == '$adminUser' and adminPass == '$adminPass' limit 1";
+            $result = $this->db->select($query);
+            if($result != false){
+                $value = $result->fetch_assoc();
+                Session::set('adminlogin' ,true);
+                Session::set('adminid',$value['adminid']);
+                Session::set('adminUser',$value['adminUser']);
+                Session::set('adminName',$value['adminName']);
+                header('Location:index.php');
+            }else{
+                $alert = "User and Pass not macth";
+                return $alert;
+            }
+        }
+    }
+}
+
+?>
